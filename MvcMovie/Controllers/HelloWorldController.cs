cd3fb8854a3f39cb4d8ee9aa0af6bcb09bd34aee
tanviriqbal <tanviriqbal4u@live.com>
2018-05-12 19:41:18 +0600

    Adding a view

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -6,37 +6,19 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // 
-        // GET: /HelloWorld/ 
+        // GET: /HelloWorld/
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        //public string Welcome()
-        //{
-        //    return "This is the Welcome action method...";
-        //}
-
-        // 
-        // GET: /HelloWorld/Welcome/?name=Scott&numtimes=4
-        //The ? (question mark) in the above URL is a separator, and the query strings follow. The & character separates query strings.
-        //public string Welcome(string name, int numTimes = 1)
-        //{
-        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
-        //}
-
-        // 
-        // GET: /HelloWorld/Welcome/1?name=Scott  
-        //This time the third URL segment matched the route parameter ID. The Welcome action method contains a parameter (ID) that matched the URL specification in the RegisterRoutes method.
-        // In ASP.NET MVC applications, it's more typical to pass in parameters as route data (like we did with ID above) than passing them as query strings.
-        // GET: /HelloWorld/Welcome/Scott/
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 * M-Models: Classes that represent the data of the application and that use validation logic to enforce business rules for that data.
 * V-Views: Template files that your application uses to dynamically generate HTML responses.
 * C-Controllers: Classes that handle incoming browser requests, retrieve model data, and then specify view templates that return a response to the browser.
+
+## Razor view engine
+
+Razor-based view templates have a .cshtml file extension, and provide an elegant way to create HTML output using C#. Razor minimizes the number of characters and keystrokes required when writing a view template, and enables a fast, fluid coding workflow.
